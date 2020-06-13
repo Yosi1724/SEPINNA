b6773aedf7cd39bc4ca6959e04656e7815c91dda
Yosijaki <yosialbino24@gmail.com>
2020-06-13 09:23:14 -0500

    Primer commit del release

---
+++ pagina_release.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+	include_once 'dbconnect.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PAGINA RELEASE</title>
+	<meta content="width=device-width, initial-scale=1.0" name="viewport" >
+	<link rel="stylesheet" href="Css/margenes.css" type="text/css" />
+	<link rel="stylesheet" href="Css/bootstrap.min.css" type="text/css" />
+	<link rel="stylesheet" href="css/.css" type="text/css" />
+
+    <link rel="icon" href="/Imagenes/Gobierno/Logo-Silvano.png"  type="image/png" />
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+</head>
+
+<body>
+	
+	<h2> Contacta con los mejores psicologos</h2>
+	
+</body>
