9c40e14d884d640c78d89b5f5b61fe3105ad3d34
Yosijaki <yosialbino24@gmail.com>
2020-06-13 10:10:04 -0500

    Editamos el index en la rama master

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 	<div class="container-fluid">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
-				<span class="sr-only">Toggle navigation</span>
+				<span class="sr-only">Navegacion de derechos</span>
 				<span class="icon-bar"></span>
 			</button>
 			<a class="navbar-brand" href="index.php">Secretaría Ejecutiva del Sistema Estatal de Protección Integral de Niñas, Niños y Adolecentes</a>
